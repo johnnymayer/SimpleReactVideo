6db44069b176076c6aca856ec4e881d7947dd9de
Johnny Mayer <johnnymayer@gmail.com>
2018-06-19 11:40:39 -0700

    Initial commit with skeletal framework.

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const App = () => {
+    return <div>Henlo</div>;
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
